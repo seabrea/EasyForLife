9879e6185ac4725da03d267f5d079ef2d1c4615d
Bob <hgdigm@gmail.com>
2019-06-13 16:27:25 +0800

    创建项目

---
+++ EasyForLife/EasyForLife/HomeTabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeTabBarController.swift
+//  EasyForLife
+//
+//  Created by Bob on 2019/6/13.
+//  Copyright © 2019 hgd. All rights reserved.
+//
+
+import UIKit
+
+class HomeTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
